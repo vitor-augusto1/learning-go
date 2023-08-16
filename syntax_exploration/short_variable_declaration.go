ed6fb5e8e6c4cdabefe3a03e905992704b203f7b
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-16 15:17:42 -0300

    Short variable declaration in GO

---
+++ syntax_exploration/short_variable_declaration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  numCars := 10
+  temperature := 0.0
+  isFunny := true
+  congrats := "Happy birthday!"
+  fmt.Println(congrats)
+  fmt.Println(temperature)
+  fmt.Println(isFunny)
+  fmt.Println(numCars)
+}
