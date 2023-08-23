e07795b54c8466f89e6fb12038e8378629e7d986
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-08-23 19:22:10 -0300

    feat: Loading the PORT number from the .env file

--- RSS_aggregator_project/main.go
+++ RSS_aggregator_project/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+  "github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+  godotenv.Load(".env")
+
+  portString := os.Getenv("PORT")
+  if portString == "" {
+    log.Fatal("PORT is not found in the environment.")
+  }
+
 }
